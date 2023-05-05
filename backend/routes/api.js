837127907bed9563ca9b8de9c5590d2c50679881
Harry Wang <guanqinw@student.unimelb.edu.au>
2023-05-05 22:22:17 +1000

    add basic routers for the server

---
+++ backend/routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const apiController = require('../controllers/apiController');
+
+// *************************************************************************************** //
+//                              Define the Routes                                         //
+// *************************************************************************************** //
+
+
+module.exports = router;
---
+++ backend/routes/scenario.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const scenarioController = require('../controllers/scenarioController');
+
+// *************************************************************************************** //
+//                              Define the Routes                                         //
+// *************************************************************************************** //
+
+// Scenarios
+router.get('/', scenarioController.getPage);
+
+module.exports = router;
