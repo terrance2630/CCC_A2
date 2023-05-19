746d9263a2b7d1fc54345eafbbbcefdae0bdbfcf
Eunice55 <Eunice_smart@outlook.com>
2023-05-19 23:30:57 +1000

    analyze processed data and change sudo data

--- sudo_data/scenario_4/clean_sudo_4.py
+++ sudo_data/scenario_4/clean_sudo_4.py
@@ -5,8 +5,9 @@ import json
 # method to extract target information from sudo
 df = pd.read_csv('/Users/euniceyao/Documents/GitHub/CCC_A2/sudo_data/scenario_4/abs_2021census_g02_aust_sa4-2674477944722387812.csv')
 
+# print(df.columns)
 
-columns_to_extract = [' sa4_code_2021', ' median_tot_fam_inc_weekly', ' median_age_persons']
+columns_to_extract = [' sa4_code_2021', ' median_tot_prsnl_inc_weekly', ' median_age_persons']
 extracted_data = df.loc[:, columns_to_extract]
 
 
@@ -17,7 +18,7 @@ cleaned_dict = {}
 for index, row in extracted_data.iterrows():
     
     SA4_code = int(row[' sa4_code_2021'])
-    income = int(row[' median_tot_fam_inc_weekly'])
+    income = int(row[' median_tot_prsnl_inc_weekly'])
     age = int(row[' median_age_persons'])
     
     cleaned_dict[SA4_code] = [income, age]
