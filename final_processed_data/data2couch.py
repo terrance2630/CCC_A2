f3065111305cba94858ada0971456d2d5150ddcf
Eunice55 <Eunice_smart@outlook.com>
2023-05-22 23:32:34 +1000

    final data

--- final_processed_data/data2couch.py
+++ final_processed_data/data2couch.py
@@ -6,14 +6,14 @@ password = "group202023"
 url = f'http://{admin}:{password}@172.26.128.48:5984/'
 couch = couchdb.Server(url)
 
-db_name = 'sport_map2_positive_sentiment_towards_sport_by_location'
+db_name = 'sport_plot2_tweet_num'
 
 if db_name not in couch:
     db = couch.create(db_name)
 else:
     db = couch[db_name]
 
-with open('/Users/euniceyao/Documents/GitHub/CCC_A2/final_processed_data/positive_sentiment_towards_sport_by_loc.json') as file:
+with open('/Users/euniceyao/Documents/GitHub/CCC_A2/final_processed_data/extra_graph_data/sport_tweet_pp.json') as file:
     for line in file:
         data_dict = json.loads(line)
         
